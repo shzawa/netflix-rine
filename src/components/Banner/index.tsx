576ec713574494ed36289ed6677ace40ac2365a9
Shotaro Ozawa <39585292+shzawa@users.noreply.github.com>
2022-05-12 20:32:19 +0900

    feat: Chapter5

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,40 @@
+import { Urls } from "../../consts/urls";
+import { useRandomMovie } from "../../hooks/useRandomMovie";
+import styles from "./index.module.scss";
+
+export const Banner = () => {
+  const { movie } = useRandomMovie();
+  function truncate(str: any, n: number) {
+    // undefinedを弾く
+    if (str !== undefined) {
+      return str.length > n ? str?.substr(0, n - 1) + "..." : str;
+    }
+  }
+
+  return (
+    <header
+      className={styles["banner"]}
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("${Urls.Base}${movie?.backdrop_path}")`,
+        backgroundPosition: "center center"
+      }}
+    >
+      <div className={styles["banner-contents"]}>
+        <h1 className={styles["banner-title"]}>
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className={styles["banner-buttons"]}>
+          <button className={styles["banner-button"]}>Play</button>
+          <button className={styles["banner-button"]}>My List</button>
+        </div>
+
+        <h1 className={styles["banner-description"]}>
+          {truncate(movie?.overview, 150)}
+        </h1>
+      </div>
+
+      <div className={styles["banner-fadeBottom"]} />
+    </header>
+  );
+};
---
+++ src/hooks/useRandomMovie.ts
@@ -0,0 +1,33 @@
+import { request } from "https";
+import { useEffect, useState } from "react";
+import { Urls } from "../consts/urls";
+import { apiClient } from "../libs/api";
+
+type MovieProps = {
+  title?: string;
+  name?: string;
+  original_name?: string;
+  backdrop_path?: string;
+  overview?: string;
+};
+
+export const useRandomMovie = () => {
+  const [movie, setMovie] = useState<MovieProps>({});
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await apiClient.get(Urls.NetflixOriginals);
+      console.log(data.result);
+
+      // apiからランダムで値を取得している
+      setMovie(
+        data.results[Math.floor(Math.random() * data.results.length - 1)]
+      );
+      return request;
+    }
+    fetchData();
+  });
+
+  return {
+    movie
+  };
+};
